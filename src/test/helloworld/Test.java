fb85b8a637bd8eb0447ffca06a8089d40caab759
xiaocaibin <1032696026@qq.com>
2018-08-22 17:16:23 +0800

    66666

--- src/test/helloworld/Test.java
+++ src/test/helloworld/Test.java
@@ -7,8 +7,7 @@ public class Test {
             System.out.println("11111");
 	        System.out.println("22222");
 	        System.out.println("33333");
-
-	        
+	        System.out.println("44444");	        
 	        
 	}
 
